e9ccfc0e6a47b4675f1667c3c713c7bde9abd6da
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-17 07:12:23 +0530

    emit events and capture

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+// if user open the page start a sockets service
 io.on('connection', function(socket) {
   console.log('a user has connected');
+  
+  // alert on disconnect
   socket.on('disconnect', function() {
     console.log('a user has disconnected');
+  });
+
+  socket.on('chat message', function(msg) {
+    console.log("message:" +  msg);
   });
 });
 
